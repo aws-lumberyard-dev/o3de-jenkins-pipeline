1b6722e827dc326dfc36b828b9a3e2628c1d58c3
Brian Herrera <briher@amazon.com>
2023-07-06 16:37:19 -0700

    Add asv periodic job
    Signed-off-by: Brian Herrera <briher@amazon.com>

---
+++ jobdsl/o3de_atom_sampleviewer_periodic_clean-daily_internal.groovy
@@ -0,0 +1,66 @@
+multibranchPipelineJob('o3de-atom-sampleviewer_periodic-clean-daily-internal') {
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('o3de-atom-sampleviewer_periodic-clean-daily-internal')
+                    configuredByUrl(false)
+                    credentialsId('github')
+                    repoOwner('aws-lumberyard-dev')
+                    repository('o3de-atom-sampleviewer')
+                    repositoryUrl('https://github.com/aws-lumberyard-dev/o3de-atom-sampleviewer.git')
+                    traits {
+                        authorInChangelogTrait()
+                        gitHubBranchDiscovery {
+                            strategyId(3)
+                        }
+                        pruneStaleBranchTrait()
+                        pruneStaleTagTrait()
+                    }
+                }
+            }
+            strategy {
+                namedBranchesDifferent {
+                    defaultProperties {
+                        suppressAutomaticTriggering {
+                            triggeredBranchesRegex('^$')
+                        }
+                    }
+                    namedExceptions {
+                        named {
+                            name('development')
+                        }
+                    }
+                }
+            }
+        }
+    }
+    displayName('O3DE LY-Fork [Periodic Daily Incremental]')
+    factory {
+        remoteJenkinsFileWorkflowBranchProjectFactory {
+            fallbackBranch('development')
+            remoteJenkinsFileSCM {
+                gitSCM {
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            url('https://github.com/aws-lumberyard-dev/o3de.git')
+                            credentialsId('github')
+                        }
+                    branches {
+                        branchSpec {
+                            name('development')
+                        }
+                    }
+                    }
+                }
+            }
+            scriptPath('scripts/build/Jenkins/Jenkinsfile')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(7)
+            numToKeep(14)
+        }
+    }
+}

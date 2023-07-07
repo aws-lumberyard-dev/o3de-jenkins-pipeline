92d15b291b923bd5bd2a014a799fa2d68725d777
Brian Herrera <briher@amazon.com>
2023-07-07 13:49:46 -0700

    Add LFS job
    Signed-off-by: Brian Herrera <briher@amazon.com>

---
+++ jobdsl/LFS.groovy
@@ -0,0 +1,251 @@
+folder('LFS')
+
+freeStyleJob('CreateLFS-aws-lumberyard') {
+    label('ubuntu-command')
+    parameters {
+        stringParam('ORG', 'aws-lumberyard', '')
+        stringParam('REPO', '', '')
+    }
+    properties {
+        authorizationMatrix {
+            inheritanceStrategy {
+                inheriting()
+                permissions([
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Create:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Delete:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Update:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.View:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Build:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Cancel:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Configure:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Delete:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Discover:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Move:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Read:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Workspace:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Delete:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Replay:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Update:${/jenkins/config/team}',
+                    'GROUP:hudson.scm.SCM.Tag:${/jenkins/config/team}'
+                ])
+            }
+        }
+    }
+    scm {
+        git {
+            branch('*/s3-permissions-fix')
+            remote {
+                credentials('github')
+                url('https://github.com/amzn-changml/github-aws-lfs-service.git')
+            }
+        }
+    }
+    steps {
+        shell('''
+            #!/bin/bash
+
+            echo ***Setting up the environment
+
+            sudo apt-get install -y python-is-python3
+            sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common unzip jq 
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
+            sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+            sudo apt-get update
+            sudo apt-get install -y docker-ce
+            sudo service docker start
+            sudo usermod -a -G docker lybuilder
+
+            curl -OL https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+            unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+            sudo ./sam-installation/install
+            sam --version  && rm -rf ./sam-installation
+
+            cd github_lfs_service/lfs_lambda
+            sudo sam build --use-container
+
+            cd ../
+
+            sudo docker run --rm -v $(pwd):/tmp/layer lambci/yumda:2 bash -c "curl -sSf 'https://packagecloud.io/install/repositories/github/git-lfs/config_file.repo?os=amzn&dist=2&source=script' > /lambda/etc/yum.repos.d/github_git-lfs.repo && yum -q makecache -y --disablerepo='*' --enablerepo='github_git-lfs' --enablerepo='github_git-lfs-source' && yum install -y git git-lfs && mv /lambda/usr/bin/git-lfs /lambda/opt/bin/git-lfs && cd /lambda/opt && zip -yr /tmp/layer/git_layer.zip ."
+
+            cp -r ${WORKSPACE}/github_lfs_service/lfs_lambda/.aws-sam/build/LFSFunction/* ${WORKSPACE}/github_lfs_service/lfs_lambda/       
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            echo ***Setting up CDK environment
+
+            python -m pip install -r ${WORKSPACE}/requirements.txt
+
+            python -m pip install pytest
+
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+            export NVM_DIR="$HOME/.nvm"
+            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+            [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+            nvm install node
+
+            npm install -g aws-cdk
+
+            sudo ln -sf $(which node) /usr/local/bin/node
+            sudo ln -sf $(which cdk) /usr/local/bin/cdk
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            #echo ***Running tests
+
+            #python -m pytest
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            echo ***Getting Cloudfront certs
+
+            echo "$(aws ssm get-parameter --region us-east-1 --name /github-lfs-service/all_private_key --with-decryption | jq -r '.Parameter.Value')" | openssl rsa -pubout -out all_public_key.pem
+            echo "$(aws ssm get-parameter --region us-east-1 --name /github-lfs-service/private_key --with-decryption | jq -r '.Parameter.Value')" | openssl rsa -pubout -out get_public_key.pem
+
+            #cdk synth --all
+        '''.stripIndent().trim())
+        shell('''
+            echo ***Creating LFS infra for ${ORG}/${REPO}
+
+            rm -rf ${WORKSPACE}/cdk.out # https://github.com/aws/aws-cdk/issues/13131#issuecomment-781921888
+
+            sed -e "s/ExampleOwner/${ORG}/g" -e "s/ExampleRepository/${REPO}/g" ${WORKSPACE}/default-app-config.json > ${ORG}-${REPO}-app-config.json
+
+            cdk deploy --require-approval never --all -c config=${ORG}-${REPO}-app-config.json 
+        '''.stripIndent().trim())
+    }
+    wrappers {
+        timestamps()
+    }
+}
+
+freeStyleJob('CreateLFS-Internal') {
+    label('ubuntu-command')
+    parameters {
+        stringParam('ORG', 'aws-lumberyard', '')
+        stringParam('REPO', '', '')
+    }
+    properties {
+        authorizationMatrix {
+            inheritanceStrategy {
+                inheriting()
+                permissions([
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Create:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Delete:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.Update:${/jenkins/config/team}',
+                    'GROUP:com.cloudbees.plugins.credentials.CredentialsProvider.View:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Build:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Cancel:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Configure:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Delete:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Discover:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Move:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Read:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Item.Workspace:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Delete:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Replay:${/jenkins/config/team}',
+                    'GROUP:hudson.model.Run.Update:${/jenkins/config/team}',
+                    'GROUP:hudson.scm.SCM.Tag:${/jenkins/config/team}'
+                ])
+            }
+        }
+    }
+    scm {
+        git {
+            branch('*/main')
+            remote {
+                credentials('github')
+                url('https://github.com/aws-lumberyard/github-aws-lfs-service.git')
+            }
+        }
+    }
+    steps {
+        shell('''
+            #!/bin/bash
+
+            echo ***Setting up the environment
+
+            sudo apt install -y apt-transport-https ca-certificates curl software-properties-common unzip jq python3 python3-pip python-is-python3
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
+            sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+            sudo apt update
+            sudo apt install -y docker-ce
+            sudo service docker start
+            sudo usermod -a -G docker lybuilder
+
+            curl -OL https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+            unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+            sudo ./sam-installation/install
+            sam --version  && rm -rf ./sam-installation
+
+            cd github_lfs_service/lfs_lambda
+            sudo sam build --use-container
+
+            cd ../
+
+            sudo docker run --rm -v $(pwd):/tmp/layer lambci/yumda:2 bash -c "curl -sSf 'https://packagecloud.io/install/repositories/github/git-lfs/config_file.repo?os=amzn&dist=2&source=script' > /lambda/etc/yum.repos.d/github_git-lfs.repo && yum -q makecache -y --disablerepo='*' --enablerepo='github_git-lfs' --enablerepo='github_git-lfs-source' && yum install -y git git-lfs && mv /lambda/usr/bin/git-lfs /lambda/opt/bin/git-lfs && cd /lambda/opt && zip -yr /tmp/layer/git_layer.zip ."
+
+            cp -r ${WORKSPACE}/github_lfs_service/lfs_lambda/.aws-sam/build/LFSFunction ${WORKSPACE}/github_lfs_service/lfs_lambda/ 
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            echo ***Setting up CDK environment
+
+            python -m pip install -r ${WORKSPACE}/requirements.txt
+
+            python -m pip install pytest
+
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+            export NVM_DIR="$HOME/.nvm"
+            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+            [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+            nvm install node
+
+            npm install -g aws-cdk
+
+            sudo ln -sf $(which node) /usr/local/bin/node
+            sudo ln -sf $(which cdk) /usr/local/bin/cdk
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            echo ***Running tests
+
+            python -m pytest
+        '''.stripIndent().trim())
+        shell('''
+            #!/bin/bash
+
+            get_param() {
+                P=$(aws ssm get-parameter --region us-east-1 --name "$1" --with-decryption | jq -r '.Parameter.Value')
+                echo $P
+            }
+
+            echo ***Getting Cloudfront certs
+
+            echo "$(aws ssm get-parameter --region us-east-1 --name /github-lfs-service/all_private_key --with-decryption | jq -r '.Parameter.Value')" | openssl rsa -pubout -out all_public_key.pem
+            echo "$(aws ssm get-parameter --region us-east-1 --name /github-lfs-service/private_key --with-decryption | jq -r '.Parameter.Value')" | openssl rsa -pubout -out get_public_key.pem
+
+            cdk synth --all
+        '''.stripIndent().trim())
+        shell('''
+            echo ***Creating LFS infra for ${ORG}/${REPO}
+
+            rm -rf ${WORKSPACE}/cdk.out # https://github.com/aws/aws-cdk/issues/13131#issuecomment-781921888
+
+            sed -e "s/ExampleOwner/${ORG}/g" -e "s/ExampleRepository/${REPO}/g" ${WORKSPACE}/default-app-config.json > ${ORG}-${REPO}-app-config.json
+
+            cdk deploy --require-approval never --all -c config=${ORG}-${REPO}-app-config.json
+        '''.stripIndent().trim())
+    }
+    wrappers {
+        timestamps()
+    }
+}

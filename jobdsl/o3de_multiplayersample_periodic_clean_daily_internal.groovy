ac480f6f275a734bc1f5e82c932abadfb9f6096b
Brian Herrera <briher@amazon.com>
2023-07-07 10:46:37 -0700

    Add multiplayer sample job
    Signed-off-by: Brian Herrera <briher@amazon.com>

---
+++ jobdsl/o3de_multiplayersample_periodic_clean_daily_internal.groovy
@@ -0,0 +1,62 @@
+multibranchPipelineJob('o3de-multiplayersample_periodic-clean-daily-internal') {
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('o3de-multiplayersample_periodic-clean-daily-internal')
+                    configuredByUrl(false)
+                    credentialsId('github')
+                    repoOwner('aws-lumberyard-dev')
+                    repository('o3de-multiplayersample')
+                    repositoryUrl('https://github.com/aws-lumberyard-dev/o3de-multiplayersample.git')
+                    traits {
+                        authorInChangelogTrait()
+                        gitHubBranchDiscovery {
+                            strategyId(3)
+                        }
+                        pruneStaleBranchTrait()
+                        pruneStaleTagTrait()
+                    }
+                }
+            }
+            strategy {
+                namedBranchesDifferent {
+                    defaultProperties {
+                        suppressAutomaticTriggering {
+                            triggeredBranchesRegex('^$')
+                        }
+                    }
+                }
+            }
+        }
+    }
+    description('Multiplayer Sample based on Spectra Technology')
+    factory {
+        remoteJenkinsFileWorkflowBranchProjectFactory {
+            fallbackBranch('development')
+            matchBranches(true)
+            remoteJenkinsFileSCM {
+                gitSCM {
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            url('https://github.com/aws-lumberyard-dev/o3de.git')
+                            credentialsId('github')
+                        }
+                    branches {
+                        branchSpec {
+                            name('development')
+                        }
+                    }
+                    }
+                }
+            }
+            scriptPath('scripts/build/Jenkins/Jenkinsfile')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(7)
+            numToKeep(14)
+        }
+    }
+}

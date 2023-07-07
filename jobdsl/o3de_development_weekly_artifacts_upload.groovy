81247d386ac5be32764e2f1736e9af53f9b0b901
Brian Herrera <briher@amazon.com>
2023-07-07 13:57:30 -0700

    Add weekly artifact upload job
    Signed-off-by: Brian Herrera <briher@amazon.com>

---
+++ jobdsl/o3de_development_weekly_artifacts_upload.groovy
@@ -0,0 +1,40 @@
+pipelineJob('O3DE-LY-Fork-development_weekly-artifacts-upload') {
+    definition {
+        cpsScm {
+            lightweight(true)
+            scm {
+                git {
+                    branch('development')
+                    extensions {
+                        pruneStaleBranch()
+                        pruneTags {
+                            pruneTags(true)
+                        }
+                    }
+                    remote {
+                        credentials('github')
+                        url('https://github.com/aws-lumberyard-dev/o3de.git')
+                    }
+                }
+            }
+            scriptPath('scripts/build/Jenkins/Jenkinsfile')
+        }
+    }
+    displayName('O3DE Fork [Periodic Clean Weekly] Development')
+    logRotator {
+        daysToKeep(60)
+        numToKeep(14)
+    }
+    properties {
+        disableConcurrentBuilds {
+            abortPrevious(false)
+        }
+        pipelineTriggers {
+            triggers {
+                cron {
+                    spec('TZ=America/Los_Angeles \nH 21 * * 0 ')
+                }
+            }
+        }
+    }
+}
